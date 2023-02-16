7b87684cd315f98d78605062a372c1c60d3375fb
Tim Mewe <tim.mewe@me.com>
2023-02-16 11:27:32 +0100

    Add login

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:jambu/firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseUIAuth.configureProviders([
+    EmailAuthProvider(),
+  ]);
   runApp(const MyApp());
 }
 
@@ -33,8 +38,35 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: const Center(
-        child: Text('Home'),
+      body: SignInScreen(
+        actions: [
+          AuthStateChangeAction<SignedIn>((context, state) {
+            final loggedIn = !state.user!.emailVerified;
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (_) => LoggedInPage(
+                  isLoggedIn: loggedIn,
+                ),
+              ),
+            );
+          })
+        ],
+      ),
+    );
+  }
+}
+
+class LoggedInPage extends StatelessWidget {
+  final bool isLoggedIn;
+
+  const LoggedInPage({required this.isLoggedIn, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(isLoggedIn ? 'Yeah' : 'Meeh'),
       ),
     );
   }
