33b90c8718baa97f0f0492435a5f9a0304568812
Tim Mewe <tim.mewe@me.com>
2023-02-19 12:03:17 +0100

    Add firebase hosting

--- lib/main.dart
+++ lib/main.dart
@@ -46,9 +46,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'jambu',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
       home: HomePage(fcmToken: fcmToken),
     );
