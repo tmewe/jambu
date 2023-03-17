073f0fc09a7289e43a9779e0b81929fee922ad50
Tim Mewe <>
2023-03-17 16:43:00 +0100

    Clear search bar

--- lib/calendar/widgets/search_bar.dart
+++ lib/calendar/widgets/search_bar.dart
@@ -8,29 +8,44 @@ class SearchBar extends StatelessWidget {
   });
 
   final TextEditingController controller;
-  final ValueChanged<String>? onChanged;
+  final ValueChanged<String> onChanged;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
-      decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(4),
-          borderSide: BorderSide.none,
-        ),
-        contentPadding: const EdgeInsets.all(16),
-        filled: true,
-        hintText: 'Suche',
-        prefixIcon: const Icon(
-          Icons.search,
-          size: 24,
-        ),
-        suffixIcon: const Icon(
-          Icons.close,
-          size: 24,
-        ),
-      ),
+      controller: controller,
+      decoration: _inputDecoration,
       onChanged: onChanged,
+    );
+  }
+
+  InputDecoration get _inputDecoration {
+    return InputDecoration(
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: BorderSide.none,
+      ),
+      contentPadding: const EdgeInsets.all(16),
+      filled: true,
+      hintText: 'Suche',
+      prefixIcon: const Icon(
+        Icons.search,
+        size: 24,
+      ),
+      suffixIcon: controller.text.isNotEmpty ? _clearButton : null,
+    );
+  }
+
+  Widget get _clearButton {
+    return InkWell(
+      child: const Icon(
+        Icons.close,
+        size: 24,
+      ),
+      onTap: () {
+        onChanged('');
+        controller.clear();
+      },
     );
   }
 }
