e22e7a0cd3e231d78570fda0478a0ecb01c77283
Tim Mewe <>
2023-04-02 16:10:23 +0200

    Add padding to tag filter

--- lib/calendar/widgets/tag_filter.dart
+++ lib/calendar/widgets/tag_filter.dart
@@ -7,29 +7,34 @@ class TagFilter extends StatelessWidget {
     required this.tags,
     required this.selectedTags,
     required this.onSelectTag,
+    this.padding = EdgeInsets.zero,
     super.key,
   });
 
   final List<String> tags;
+  final EdgeInsets padding;
   final List<String> selectedTags;
   final ChipTapCallback onSelectTag;
 
   @override
   Widget build(BuildContext context) {
-    return Wrap(
-      spacing: 5,
-      runSpacing: 5,
-      crossAxisAlignment: WrapCrossAlignment.center,
-      children: [
-        const Icon(Icons.sell),
-        ...tags.map((tag) {
-          return FilterChip(
-            label: Text(tag),
-            selected: selectedTags.contains(tag),
-            onSelected: (isSelected) => onSelectTag(tag, isSelected),
-          );
-        })
-      ],
+    return Padding(
+      padding: padding,
+      child: Wrap(
+        spacing: 5,
+        runSpacing: 5,
+        crossAxisAlignment: WrapCrossAlignment.center,
+        children: [
+          const Icon(Icons.sell),
+          ...tags.map((tag) {
+            return FilterChip(
+              label: Text(tag),
+              selected: selectedTags.contains(tag),
+              onSelected: (isSelected) => onSelectTag(tag, isSelected),
+            );
+          })
+        ],
+      ),
     );
   }
 }
