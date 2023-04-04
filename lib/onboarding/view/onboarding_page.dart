273ca97139ba9d86425de9689eda2ae4ced9bb51
Tim Mewe <unknown>
2023-04-04 17:56:57 +0200

    Add onboarding file structure

---
+++ lib/onboarding/view/onboarding_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:jambu/onboarding/view/onboarding_view.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const OnboardingView();
+  }
+}
