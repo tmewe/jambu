d87f525ba3b2bef9119fa3c19d5eb454f8e211f3
Tim Mewe <unknown>
2023-04-04 20:17:26 +0200

    Add page view and basic layouting

--- lib/onboarding/view/onboarding_view.dart
+++ lib/onboarding/view/onboarding_view.dart
@@ -2,23 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jambu/onboarding/bloc/onboarding_bloc.dart';
 
-class OnboardingView extends StatelessWidget {
+class OnboardingView extends StatefulWidget {
   const OnboardingView({super.key});
+
+  @override
+  State<OnboardingView> createState() => _OnboardingViewState();
+}
+
+class _OnboardingViewState extends State<OnboardingView> {
+  final _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Onboarding'),
-            TextButton(
-              onPressed: () =>
-                  context.read<OnboardingBloc>().add(OnboardingCompleted()),
-              child: const Text('Fertig'),
-            ),
-          ],
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxHeight: 600),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: PageView(
+                  controller: _pageController,
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: [
+                    Center(
+                      child: ConstrainedBox(
+                        constraints: const BoxConstraints(maxWidth: 800),
+                        child: const Padding(
+                          padding: EdgeInsets.all(8),
+                          child: SizedBox.expand(
+                            child: Material(
+                              color: Colors.grey,
+                              child: Text('1'),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
