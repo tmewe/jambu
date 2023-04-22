a0792d9a5dc3f34107b140b723993e314e867ea8
Tim Mewe <>
2023-04-22 16:16:40 +0200

    Remove types from constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,29 +1,29 @@
 abstract class Constants {
   // Outlook
-  static const String officeEventSubject = '💼 Im Büro';
-  static const String germanTimeZone = 'Europe/Berlin';
-  static const String favoritesCalendarName = 'Favoriten (jambu)';
+  static const officeEventSubject = '💼 Im Büro';
+  static const germanTimeZone = 'Europe/Berlin';
+  static const favoritesCalendarName = 'Favoriten (jambu)';
 
   // Collections
-  static const String usersCollection = 'users';
-  static const String attendancesCollection = 'attendances';
+  static const usersCollection = 'users';
+  static const attendancesCollection = 'attendances';
 
   // Fields
-  static const String presentField = 'present';
-  static const String absentField = 'absent';
-  static const String tagsField = 'tags';
-  static const String favoritesField = 'favorites';
-  static const String onboardingCompletedField = 'onboardingCompleted';
-  static const String explanationsCompleted = 'explanationsCompleted';
-  static const String regularAttendancesField = 'regularAttendances';
+  static const presentField = 'present';
+  static const absentField = 'absent';
+  static const tagsField = 'tags';
+  static const favoritesField = 'favorites';
+  static const onboardingCompletedField = 'onboardingCompleted';
+  static const explanationsCompleted = 'explanationsCompleted';
+  static const regularAttendancesField = 'regularAttendances';
 
   // FCM
-  static const String vapidKey =
+  static const vapidKey =
       // ignore: lines_longer_than_80_chars
       'BDwDEXNpZUq9IJQ60LNTt3At9ctSWMBiEo5BMXzB9X2VojyfM0En84zNMr328DhLhruGVJQPCjo2lTJ3YCZhGoY';
 
   // TODO(tim): REMOVE
-  static const String testCalendarId =
+  static const testCalendarId =
       // ignore: lines_longer_than_80_chars
       'AAMkAGYwY2Y0MWM4LWE2MzItNDk5Ny05NzIzLWFjNjUwZjI3Y2UwYwBGAAAAAABd4EEhe61iSIEnLzkh3SdoBwDnm8A17Q_oQ41X7GXJE69AAAAAAAEGAADnm8A17Q_oQ41X7GXJE69AAACKlQf_AAA=';
 }
