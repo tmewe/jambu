733587cbe49cddc1268afdb240441fb979b3d1ed
Tim Mewe <unknown>
2023-04-28 14:13:14 +0200

    Replace switch with checkmark button

--- lib/calendar/widgets/calendar_day_overview.dart
+++ lib/calendar/widgets/calendar_day_overview.dart
@@ -1,3 +1,4 @@
+import 'package:app_ui/app_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
@@ -28,23 +29,63 @@ class CalendarDayOverview extends StatelessWidget {
       children: [
         Text(isBestChoice ? 'Optimaler Tag' : ''),
         Text(reason),
-        Switch(
-          value: day.isUserAttending,
-          onChanged: !day.isHoliday
-              ? (value) {
-                  context.read<CalendarBloc>().add(
-                        CalendarAttendanceUpdate(
-                          date: day.date.midnight,
-                          isAttending: value,
-                          reason: day.reason,
-                        ),
-                      );
-                }
-              : null,
+        _CheckmarkButton(
+          isSelected: day.isUserAttending,
+          onTap: (bool value) {
+            context.read<CalendarBloc>().add(
+                  CalendarAttendanceUpdate(
+                    date: day.date.midnight,
+                    isAttending: value,
+                    reason: day.reason,
+                  ),
+                );
+          },
         ),
         Text(day.date.weekdayString),
         Text(DateFormat('dd').format(day.date)),
       ],
+    );
+  }
+}
+
+class _CheckmarkButton extends StatelessWidget {
+  const _CheckmarkButton({
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  final bool isSelected;
+  final void Function(bool) onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final iconColor = isSelected ? AppColors.white : AppColors.platinumGrey;
+    final bgColor = isSelected ? AppColors.green : AppColors.seasaltGrey;
+    final borderColor = isSelected ? AppColors.green : AppColors.platinumGrey;
+
+    return Material(
+      color: bgColor,
+      shape: const CircleBorder(),
+      child: InkWell(
+        customBorder: CircleBorder(
+          side: BorderSide(
+            width: 3,
+            color: borderColor,
+          ),
+        ),
+        onTap: () => onTap(!isSelected),
+        child: Ink(
+          width: 55,
+          height: 55,
+          decoration: const BoxDecoration(shape: BoxShape.circle),
+          child: Center(
+            child: Icon(
+              Icons.check,
+              color: iconColor,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
