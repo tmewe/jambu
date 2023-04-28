d2ecae2285e5c76a7119b0b5c6dca2ef73096d47
Tim Mewe <>
2023-04-28 13:45:06 +0200

    Refactor day column

---
+++ lib/calendar/widgets/calendar_day_users_list.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jambu/calendar/bloc/calendar_bloc.dart';
+import 'package:jambu/calendar/model/model.dart';
+import 'package:jambu/calendar/widgets/calender_item.dart';
+
+class CalendarDayUsersList extends StatelessWidget {
+  const CalendarDayUsersList({
+    required this.day,
+    required this.tags,
+    super.key,
+  });
+
+  final CalendarDay day;
+  final List<String> tags;
+
+  @override
+  Widget build(BuildContext context) {
+    var reason = day.reason ?? '';
+    if (day.isHoliday) {
+      reason = '🏖️ $reason';
+    }
+
+    final sortedUsers = day.sortedUsers;
+    return ListView.separated(
+      itemBuilder: (context, index) {
+        final user = sortedUsers[index];
+        return CalendarItem(
+          user: user,
+          tags: tags,
+          onAddTag: (tag, userId) {
+            context.read<CalendarBloc>().add(
+                  CalendarAddTag(
+                    tagName: tag,
+                    userId: user.id,
+                  ),
+                );
+          },
+          onRemoveTag: (tag, userId) {
+            context.read<CalendarBloc>().add(
+                  CalendarRemoveTag(
+                    tagName: tag,
+                    userId: user.id,
+                  ),
+                );
+          },
+          onUpdateTagName: (oldName, newName) {
+            context.read<CalendarBloc>().add(
+                  CalendarUpdateTagName(
+                    tagName: oldName,
+                    newTagName: newName,
+                  ),
+                );
+          },
+          onUpdateFavorite: (isFavorite) {
+            context.read<CalendarBloc>().add(
+                  CalenderUpdateFavorite(
+                    userId: user.id,
+                    isFavorite: isFavorite,
+                  ),
+                );
+          },
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(height: 10),
+      itemCount: sortedUsers.length,
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+    );
+  }
+}
