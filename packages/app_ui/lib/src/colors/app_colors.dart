2179277e74c506e35a9ff5c6a81ef1f4f0178c9a
Tim Mewe <>
2023-04-28 14:13:14 +0200

    Replace switch with checkmark button

--- packages/app_ui/lib/src/colors/app_colors.dart
+++ packages/app_ui/lib/src/colors/app_colors.dart
@@ -4,6 +4,7 @@ abstract class AppColors {
   static const Color orange = Color(0xffef7d00);
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
+  static const Color green = Color(0xFF8FD14F);
   
   static const Color seasaltGrey = Color(0xFFF8F9FA);
   static const Color platinumGrey = Color(0xFFDEE2E6);
