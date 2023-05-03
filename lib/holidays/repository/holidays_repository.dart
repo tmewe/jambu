ff442db804f473344911316130309ba0ff3a4bf5
Tim Mewe <>
2023-05-03 08:00:16 +0200

    Fetch holidays from start of week

--- lib/holidays/repository/holidays_repository.dart
+++ lib/holidays/repository/holidays_repository.dart
@@ -1,4 +1,5 @@
 import 'package:collection/collection.dart';
+import 'package:jambu/extension/extension.dart';
 import 'package:jambu/holidays/datasource/datasource.dart';
 import 'package:jambu/model/model.dart';
 
@@ -11,10 +12,15 @@ class HolidaysRepository {
 
   Future<List<Holiday>> fetchNationwideHolidaysInNextFourWeeks() async {
     final holidaysRaw = await _datasource.fetchHolidays();
+    final startDate =
+        DateTime.now().firstDateOfWeek.subtract(const Duration(days: 1));
+    final endDate =
+        DateTime.now().firstDateOfWeek.add(const Duration(days: 29));
+
     return holidaysRaw.map((e) => e.toModel()).whereNotNull().where((h) {
       return h.nationwide &&
-          h.date.isAfter(DateTime.now().subtract(const Duration(days: 1))) &&
-          h.date.isBefore(DateTime.now().add(const Duration(days: 29)));
+          h.date.isAfter(startDate) &&
+          h.date.isBefore(endDate);
     }).toList();
   }
 }
