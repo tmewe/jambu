02bbaca44b1ff202003bfa52e6b9576a5d380e43
Tim Mewe <unknown>
2023-05-15 19:51:34 +0200

    Add basic mobile layout

--- packages/app_ui/lib/src/layout/adaptive.dart
+++ packages/app_ui/lib/src/layout/adaptive.dart
@@ -1,2 +1,5 @@
 /// The maximum width taken up by each item on the home screen.
 const maxElementWidth = 1400.0;
+
+/// The maximum with when the mobile layout should be displayed
+const phoneBreakpoint = 800;
