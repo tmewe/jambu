af8844d1b9b1be504843ed1c91798411d5da3910
Tim Mewe <>
2023-05-15 20:46:33 +0200

    More refactoring

--- lib/calendar/view/calendar_view.dart
+++ lib/calendar/view/calendar_view.dart
@@ -35,12 +35,6 @@ class _CalendarViewState extends State<CalendarView> {
   @override
   Widget build(BuildContext context) {
     final screeWidth = MediaQuery.of(context).size.width;
-    // switch (screeWidth) {
-    //   case >= 0 && <= phoneBreakpoint:
-    //     return Text('Mobile');
-    //   default:
-    //     return Text('Desktop');
-    // }
     return BlocConsumer<CalendarBloc, CalendarState>(
       listener: (BuildContext context, CalendarState state) {
         if (state.user != null &&
@@ -52,7 +46,7 @@ class _CalendarViewState extends State<CalendarView> {
           showDialog<void>(
             barrierDismissible: false,
             context: context,
-            builder: (context) => _ExplanationsAlert(
+            builder: (context) => ExplanationsAlert(
               onCompleteTap: () {
                 bloc.add(CalenderExplanationsCompleted());
                 context.pop();
@@ -176,12 +170,14 @@ class _MobileCalendarState extends State<_MobileCalendar> {
 
   @override
   Widget build(BuildContext context) {
-    final finalDays = widget.state.filteredDays;
-    final selectedDay = finalDays[_selectedDayIndex];
-    const startDate = '1';
-    // _dateFormat.format(selectedWeek.days.first.date);
-    const endDate = '2';
-    // _dateFormat.format(selectedWeek.days.last.date);
+    final filteredWeeks = widget.state.filteredWeeks;
+    final filteredDays =
+        filteredWeeks.map((week) => week.days).expand((e) => e).toList();
+    final selectedDay = filteredDays[_selectedDayIndex];
+    final selectedWeekIndex = (_selectedDayIndex / 5).floor();
+    final selectedWeek = filteredWeeks[selectedWeekIndex];
+    final startDate = _dateFormat.format(selectedWeek.days.first.date);
+    final endDate = _dateFormat.format(selectedWeek.days.last.date);
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -192,14 +188,16 @@ class _MobileCalendarState extends State<_MobileCalendar> {
           onPreviousTap: _selectedDayIndex > 0
               ? () => setState(() => _selectedDayIndex--)
               : null,
-          onNextTap: () => setState(() => _selectedDayIndex++),
-          // ? () => setState(() => _selectedDayIndex++)
-          // : null,
+          onNextTap: _selectedDayIndex < filteredDays.length - 1
+              ? () => setState(() => _selectedDayIndex++)
+              : null,
         ),
         const SizedBox(height: 20),
         _CalendarDay(
           day: selectedDay,
-          isBestChoice: false,
+          isBestChoice: selectedWeek.bestChoices.contains(
+            selectedDay.date.weekday,
+          ),
           sortedTags: widget.state.sortedTags,
           user: widget.state.user,
         ),
@@ -489,157 +487,6 @@ class _AppBar extends StatelessWidget {
     }
     router.removeListener(
       () => _routerListener(router: router, context: context),
-    );
-  }
-}
-
-class _ExplanationsAlert extends StatefulWidget {
-  const _ExplanationsAlert({
-    required this.onCompleteTap,
-  });
-
-  final VoidCallback onCompleteTap;
-
-  @override
-  State<_ExplanationsAlert> createState() => _ExplanationsAlertState();
-}
-
-class _ExplanationsAlertState extends State<_ExplanationsAlert> {
-  final _pageController = PageController();
-
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      content: ConstrainedBox(
-        constraints: BoxConstraints.tight(const Size(550, 650)),
-        child: PageView(
-          controller: _pageController,
-          physics: const NeverScrollableScrollPhysics(),
-          children: [
-            _FavoritesExplanation(
-              onCompleteTap: () => _pageController.nextPage(
-                duration: PageTransition.duration,
-                curve: PageTransition.curve,
-              ),
-            ),
-            _TagsExplanation(
-              onBackTap: () => _pageController.previousPage(
-                duration: PageTransition.duration,
-                curve: PageTransition.curve,
-              ),
-              onCompleteTap: widget.onCompleteTap,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _FavoritesExplanation extends StatelessWidget {
-  const _FavoritesExplanation({
-    required this.onCompleteTap,
-  });
-
-  final VoidCallback onCompleteTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        const SizedBox(height: 10),
-        SelectableText(
-          'Favoriten',
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
-        const SizedBox(height: 20),
-        Image.asset('assets/images/favorites.png'),
-        const SizedBox(height: 20),
-        const SelectableText(
-          'Mit dem Herz kannst du bestimmte Kolleg*innen '
-          'favorisieren. Diese erscheinen dann immer oben '
-          'in deiner Übersicht. Außerdem zeigt dir jambu auf '
-          'Basis deiner Favoriten an, welche Tage die optimalen '
-          'Bürotage für dich wären.',
-        ),
-        const SizedBox(height: 5),
-        const SelectableText(
-          'Für einen noch schnelleren Überblick werden deine '
-          'Favoriten mit Outlook gesynct.',
-        ),
-        const Spacer(),
-        Row(
-          children: [
-            const Spacer(),
-            FilledButton(
-              onPressed: onCompleteTap,
-              child: const Text('Nice'),
-            ),
-          ],
-        ),
-      ],
-    );
-  }
-}
-
-class _TagsExplanation extends StatelessWidget {
-  const _TagsExplanation({
-    required this.onBackTap,
-    required this.onCompleteTap,
-  });
-
-  final VoidCallback onBackTap;
-  final VoidCallback onCompleteTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        const SizedBox(height: 10),
-        SelectableText(
-          'Tägs',
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
-        const SizedBox(height: 20),
-        Image.asset('assets/images/tags.png'),
-        const SizedBox(height: 20),
-        const SelectableText(
-          'Mit dem Plus kannst du bestimmten Kolleg*innen Tägs zuordnen '
-          "und auf diese Art und Weise gruppieren. Mit dem 'X' wird der Täg "
-          'von der Nutzer*in entfernt. Wenn du einen Täg komplett '
-          'löschen möchtest, geht das über dein Profil. Tägs umbennen kannst '
-          'du einfach per Klick auf den Täg.',
-        ),
-        const SizedBox(height: 5),
-        const SelectableText(
-          'Sowohl deine Favoriten, als auch deine Tägs sind personalisiert '
-          'und somit nur für dich einsehbar.',
-        ),
-        const SizedBox(height: 5),
-        const SelectableText(
-          'Für einen noch schnelleren Überblick werden deine '
-          'Tägs mit Outlook gesynct.',
-        ),
-        const Spacer(),
-        Row(
-          children: [
-            const Spacer(),
-            TextButton(
-              onPressed: onBackTap,
-              child: const Text('Zurück'),
-            ),
-            const SizedBox(width: 10),
-            FilledButton(
-              onPressed: onCompleteTap,
-              child: const Text('Fertig'),
-            ),
-          ],
-        )
-      ],
     );
   }
 }
